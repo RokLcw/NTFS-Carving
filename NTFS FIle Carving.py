52e696dfd925ab63d2bd67ddea8cb3e0873fc3fb
RokLcw <lcwlyw@naver.com>
2023-09-07 15:21:09 +0900

    Update NTFS FIle Carving.py

--- NTFS FIle Carving.py
+++ NTFS FIle Carving.py
@@ -1,101 +1,111 @@
 import sys
+import os
 import time
 
-def MFT_analysis(data, VBR_offset, MFT_offset):
-    MFT_ptr = MFT_offset
-    # print(MFT_ptr)
-    restore_list = []
-    MFT_count = 0
+def MFT_analysis(data, restore_Partition_cnt, VBR_offset, MFT_offset):
+    for partition_cnt in range(0, restore_Partition_cnt):
+        try:
+            os.mkdir(f"./result/data{partition_cnt}")
+        except Exception as e:
+            print(f"{e}")
 
-    while(True):
-        if(data[MFT_ptr:MFT_ptr+4] != b'\x46\x49\x4C\x45'):
-            if((MFT_count >= 12 and MFT_count <= 23) == 0):
-                break
-            else:
-                 MFT_ptr += 1024
-                 MFT_count += 1
-                 continue
-            
-        if(int.from_bytes(data[MFT_ptr+22:MFT_ptr+23], 'little') == 0):
-            restore_list.append(data[MFT_ptr:MFT_ptr + 1024])
+        MFT_ptr = MFT_offset[partition_cnt]
+        # print(MFT_ptr)
+        restore_list = []
+        MFT_count = 0
 
-        MFT_ptr += 1024
-        MFT_count += 1
-
-    # print(restore_list)
-    # 파일 복구
-    for i in range(0, len(restore_list)):
-        # print(len(restore_list), i)
-        # if(i == 19):
-        #     with open(f"./result/mft20.dat", "wb") as restore_file:
-        #         restore_file.write(bytes(restore_list[20]))
-        # 속성 시작 주소
-        Attribute_start_offset = int.from_bytes(restore_list[i][4:6], 'little') + ((int.from_bytes(restore_list[i][6:8], 'little') * 2) + 2)
-        # print(Attribute_start_offset)
-        # 각 속성 추적
-        Attribute_ptr = Attribute_start_offset
         while(True):
-            # MFT Entry End Marker를 이용하여 반복문 탈출 시점 결정
+            if(data[MFT_ptr:MFT_ptr+4] != b'\x46\x49\x4C\x45'):
+                if((MFT_count >= 12 and MFT_count <= 23) == 0):
+                    break
+                else:
+                    MFT_ptr += 1024
+                    MFT_count += 1
+                    continue
+                
+            if(int.from_bytes(data[MFT_ptr+22:MFT_ptr+23], 'little') == 0):
+                restore_list.append(data[MFT_ptr:MFT_ptr + 1024])
+
+            MFT_ptr += 1024
+            MFT_count += 1
+
+        # print(restore_list)
+        # 파일 복구
+        for i in range(0, len(restore_list)):
+            # print(len(restore_list), i)
             # if(i == 19):
-            #     print(restore_list[i][Attribute_ptr:Attribute_ptr+4])
-            #     print(Attribute_ptr)
-            #     time.sleep(2)
-            if(restore_list[i][Attribute_ptr:Attribute_ptr+4] == b'\xFF\xFF\xFF\xFF'):
-                break
-            if(int.from_bytes(restore_list[i][Attribute_ptr:Attribute_ptr+4], 'little') == 0x30):
-                file_name_length = (restore_list[i][Attribute_ptr + 88]) * 2
-                # print(file_name_length)
-                file_name = restore_list[i][Attribute_ptr + 90: (Attribute_ptr + 90) + file_name_length]
-                # print(file_name)
-                file_name = file_name.decode('UTF-16')
-                # print(file_name)
-                # with open(f"./result/list.txt", "a") as file_list:
-                #     file_list.write(f"{file_name}\n")
-            if(int.from_bytes(restore_list[i][Attribute_ptr:Attribute_ptr+4], 'little') == 0x80):
-                # Non-resident 방식 확인
-                if(restore_list[i][Attribute_ptr + 8] == 0x1):
-                    # 런리스트 주소 확인
-                    run_list_start_offset = int.from_bytes(restore_list[i][Attribute_ptr + 32: Attribute_ptr + 34], 'little')
-                    run_list_start_offset = Attribute_ptr + run_list_start_offset
-                    # print(run_list_start_offset)
+            #     with open(f"./result/mft20.dat", "wb") as restore_file:
+            #         restore_file.write(bytes(restore_list[20]))
+            # 속성 시작 주소
+            Attribute_start_offset = int.from_bytes(restore_list[i][4:6], 'little') + ((int.from_bytes(restore_list[i][6:8], 'little') * 2) + 2)
+            # print(Attribute_start_offset)
+            # 각 속성 추적
+            Attribute_ptr = Attribute_start_offset
+            while(True):
+                # MFT Entry End Marker를 이용하여 반복문 탈출 시점 결정
+                # if(i == 19):
+                #     print(restore_list[i][Attribute_ptr:Attribute_ptr+4])
+                #     print(Attribute_ptr)
+                #     time.sleep(2)
+                if(restore_list[i][Attribute_ptr:Attribute_ptr+4] == b'\xFF\xFF\xFF\xFF'):
+                    break
+                if(int.from_bytes(restore_list[i][Attribute_ptr:Attribute_ptr+4], 'little') == 0x30):
+                    file_name_length = (restore_list[i][Attribute_ptr + 88]) * 2
+                    # print(file_name_length)
+                    file_name = restore_list[i][Attribute_ptr + 90: (Attribute_ptr + 90) + file_name_length]
+                    # print(file_name)
+                    file_name = file_name.decode('UTF-16')
+                    # print(file_name)
+                    # with open(f"./result/list.txt", "a") as file_list:
+                    #     file_list.write(f"{file_name}\n")
+                if(int.from_bytes(restore_list[i][Attribute_ptr:Attribute_ptr+4], 'little') == 0x80):
+                    # Non-resident 방식 확인
+                    if(restore_list[i][Attribute_ptr + 8] == 0x1):
+                        # 런리스트 주소 확인
+                        run_list_start_offset = int.from_bytes(restore_list[i][Attribute_ptr + 32: Attribute_ptr + 34], 'little')
+                        run_list_start_offset = Attribute_ptr + run_list_start_offset
+                        # print(run_list_start_offset)
 
-                    # 런리스트 주소가 가리키는 최종 런리스트 주소 값.
-                    real_data_offset_calc = restore_list[i][run_list_start_offset]
-                    # print(real_data_offset_calc)
+                        # 런리스트 주소가 가리키는 최종 런리스트 주소 값.
+                        real_data_offset_calc = restore_list[i][run_list_start_offset]
+                        # print(real_data_offset_calc)
 
-                    # 런리스트 해석
-                    real_data_cluster_size = real_data_offset_calc % 0x10
-                    real_data_offset = real_data_offset_calc / 0x10
-                    # print(real_data_cluster_size, int(real_data_offset))
-                    # print(int.from_bytes(restore_list[i][run_list_start_offset+real_data_cluster_size+1:(run_list_start_offset+real_data_cluster_size+1)+int(real_data_offset)], 'little'))
+                        # 런리스트 해석
+                        real_data_cluster_size = real_data_offset_calc % 0x10
+                        real_data_offset = real_data_offset_calc / 0x10
+                        # print(real_data_cluster_size, int(real_data_offset))
+                        # print(int.from_bytes(restore_list[i][run_list_start_offset+real_data_cluster_size+1:(run_list_start_offset+real_data_cluster_size+1)+int(real_data_offset)], 'little'))
 
-                    # 런리트를 이용해 구한 실제 파일 주소와 사이즈, 클러스터 사이즈
-                    real_data_offset = int.from_bytes(restore_list[i][run_list_start_offset+real_data_cluster_size+1:(run_list_start_offset+real_data_cluster_size+1)+int(real_data_offset)], 'little')
-                    real_data_cluster_size = int.from_bytes(restore_list[i][run_list_start_offset + real_data_cluster_size: run_list_start_offset + real_data_cluster_size + 1], 'little')
-                    real_data_size = int.from_bytes(restore_list[i][run_list_start_offset-8:run_list_start_offset], 'little')
-                    # print(real_data_size)
-                    # print(real_data_cluster_size)
+                        # 런리트를 이용해 구한 실제 파일 주소와 사이즈, 클러스터 사이즈
+                        real_data_offset = int.from_bytes(restore_list[i][run_list_start_offset+real_data_cluster_size+1:(run_list_start_offset+real_data_cluster_size+1)+int(real_data_offset)], 'little')
+                        real_data_cluster_size = int.from_bytes(restore_list[i][run_list_start_offset + real_data_cluster_size: run_list_start_offset + real_data_cluster_size + 1], 'little')
+                        real_data_size = int.from_bytes(restore_list[i][run_list_start_offset-8:run_list_start_offset], 'little')
+                        # print(real_data_size)
+                        # print(real_data_cluster_size)
 
-                    # print(real_data_offset)
+                        # print(real_data_offset)
 
-                    real_data_offset = ((real_data_offset * 8)*512) + VBR_offset
+                        real_data_offset = ((real_data_offset * 8)*512) + VBR_offset[partition_cnt]
 
-                    # print(real_data_offset)
+                        # print(real_data_offset)
 
-                    try:
-                        with open(f"./result/{file_name}", "wb") as restore_file:
-                            restore_file.write(bytes(data[real_data_offset:real_data_offset+real_data_size]))
-                    except:
-                        print(f"{file_name}")
-                        
-            
-            Attribute_ptr = Attribute_ptr + int.from_bytes(restore_list[i][Attribute_ptr + 4:Attribute_ptr + 6], 'little')
+                        try:
+                            with open(f"./result/data{partition_cnt}/{file_name}", "wb") as restore_file:
+                                restore_file.write(bytes(data[real_data_offset:real_data_offset+real_data_size]))
+                        except:
+                            print(f"{file_name}")
+                            
+                
+                Attribute_ptr = Attribute_ptr + int.from_bytes(restore_list[i][Attribute_ptr + 4:Attribute_ptr + 6], 'little')
 
 def VBR_analysis(data, first_LBA, Last_LBA):
-    VBR_offset = first_LBA * 512
+    VBR_offset = []
+    MFT_offset = []
 
-    sector_per_cluster = data[VBR_offset+13]
-    MFT_offset = ((int.from_bytes(data[VBR_offset+48:VBR_offset+56], 'little') * sector_per_cluster) * 512) + VBR_offset
+    for i in range(0, len(first_LBA)):
+        VBR_offset.append(first_LBA[i] * 512)
+        sector_per_cluster = data[VBR_offset[i]+13]
+        MFT_offset.append(((int.from_bytes(data[VBR_offset[i]+48:VBR_offset[i]+56], 'little') * sector_per_cluster) * 512) + VBR_offset[i])
 
     return VBR_offset, MFT_offset
 
@@ -121,18 +131,21 @@ def GPT_analysis(data, GPT_Header_offset):
         if(activate_GPT_Partition_Entry[i][56:96] == b'\x42\x00\x61\x00\x73\x00\x69\x00\x63\x00\x20\x00\x64\x00\x61\x00\x74\x00\x61\x00\x20\x00\x70\x00\x61\x00\x72\x00\x74\x00\x69\x00\x74\x00\x69\x00\x6F\x00\x6E\x00'):
             data_restore_Partition.append(activate_GPT_Partition_Entry[i])
 
-    # for i in range(0, len(data_restore_Partition)):
-    # 임시로 한 개 파티션 고정
-    fix = data_restore_Partition[0]
+    first_LBA = []
+    Last_LBA = []
+    for i in range(0, len(data_restore_Partition)):
+        partition = data_restore_Partition[i]
 
-    first_LBA = int.from_bytes(fix[32:40], 'little')
-    Last_LBA = int.from_bytes(fix[40:48], 'little')
+        first_LBA.append(int.from_bytes(partition[32:40], 'little'))
+        Last_LBA.append(int.from_bytes(partition[40:48], 'little'))
 
-    return first_LBA, Last_LBA
+    return len(data_restore_Partition), first_LBA, Last_LBA
 
 
 
 if __name__ == '__main__':
+    start = time.time()
+
     with open(sys.argv[1], "rb") as imaging_file:
         data = imaging_file.read()
 
@@ -151,8 +164,12 @@ if __name__ == '__main__':
                 # 1 sector -> 512 bytes
                 # GPT 구조 분석
                 GPT_Header_offset = int.from_bytes(Partition_table_entry[i][8:12], 'little') * 512
-                first_LBA, Last_LBA = GPT_analysis(data, GPT_Header_offset)
+                restore_Partition_cnt, first_LBA, Last_LBA = GPT_analysis(data, GPT_Header_offset)
+                # print(first_LBA, Last_LBA)
                 # VBR 구조 분석
                 VBR_offset, MFT_offset = VBR_analysis(data, first_LBA, Last_LBA)
+                # print(VBR_offset, MFT_offset)
                 # MFT 구조 분석
-                MFT_analysis(data, VBR_offset, MFT_offset)
+                MFT_analysis(data, restore_Partition_cnt, VBR_offset, MFT_offset)
+    
+    print("time :", time.time() - start)
